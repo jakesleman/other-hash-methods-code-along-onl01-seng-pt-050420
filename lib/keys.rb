90d79bc61eb8efaccf1a621a19fb3eda154bc40e
Jake Sleman <jakesleman@icloud.com>
2020-05-17 22:25:21 +0000

    Automatically backed up by Learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -18,3 +18,19 @@ puts groceries.values.flatten.min
 def get_the_keys(groceries)
   groceries.keys
 end
+
+get_the_keys(groceries)
+
+
+def get_the_values(groceries)
+  #code your solution here!
+  groceries.values 
+  groceries.values.flatten 
+end
+
+get_the_values(groceries)
+
+
+def get_the_min(groceries)
+  groceries.values.flatten.min
+end
